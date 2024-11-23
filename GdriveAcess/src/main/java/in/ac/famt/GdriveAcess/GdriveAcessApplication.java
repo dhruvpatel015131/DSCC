24dd024f3971d6ad9276f556268a3fba0d5c4580
Dhruv Patel <140480404+dhruvpateltural@users.noreply.github.com>
2024-11-23 08:56:10 +0530

    Add files via upload

---
+++ GdriveAcess/src/main/java/in/ac/famt/GdriveAcess/GdriveAcessApplication.java
@@ -0,0 +1,13 @@
+package in.ac.famt.GdriveAcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdriveAcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdriveAcessApplication.class, args);
+	}
+
+}
